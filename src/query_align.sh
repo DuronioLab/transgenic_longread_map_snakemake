3714f2afd4fd294007f0f2eb3e4878e1df118b8e
Markus Nevil <37672597+markus-nevil@users.noreply.github.com>
2024-05-07 15:26:44 -0400

    Create query_align.sh

---
+++ src/query_align.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Assign command-line arguments to variables
+input_fastq=$1
+fasta=$2
+output_sam=$3
+output_bam=$4
+query_fasta=$5
+
+seqkit replace --quiet -p .+ -r 'seq_{nr}' $input_fastq > renamed_reads.fastq
+seqkit seq --quiet --min-len 5000 renamed_reads.fastq -o out.fastq
+seqkit fq2fa --quiet out.fastq -o out.fasta
+
+split --lines=40000 -d --additional-suffix=".fasta" out.fasta out.
+
+touch all_results.psl
+
+for fasta_file in out.*.fasta
+do
+    blat "$fasta_file" $query_fasta -oneOff=3 -noHead output.psl
+    cat output.psl >> all_results.psl
+done
+
+awk 'NR > 6 {print $14}' all_results.psl > temp_names.txt
+
+sort temp_names.txt | uniq > blat_names.txt
+
+readarray -t File < blat_names.txt
+touch filtered_reads.fasta
+for f in "${File[@]}"
+do
+  echo $f > fname.txt
+  seqkit grep --quiet -n -f fname.txt out.fasta >> filtered_reads.fasta
+done
+
+seqtk seq -F '#' filtered_reads.fasta > filtered_reads.fastq
+
+minimap2 --secondary=no --sam-hit-only -ax map-ont $fasta filtered_reads.fastq > $output_sam
+
+samtools sort $output_sam -o mapped.bam
+samtools view -bq 1 mapped.bam > $output_bam
+samtools index $output_bam
